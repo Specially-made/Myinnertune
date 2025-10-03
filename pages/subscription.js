a60ef709d3cd9150f676fd01a64b41c72b376fac
Peptide god <PHITON@DISTRICTF5VE.COM>
2025-10-03 05:33:13 -0700

    Add subscription.js to pages directory

--- pages/subscription.js
+++ pages/subscription.js
@@ -5,7 +5,7 @@ export default function Subscription() {
   const sessionId = 'cs_test_a1g6zxIJqBuh0q77zqOLJtQquyk6LmYPuhdndIMFFTBtNO4drqCaopWZuz';
 
   useEffect(() => {
-    fetch(`https://myinnertune-3ads0uhsu-tobiaswedler-2383s-projects.vercel.app/api/verify-session?session_id=${sessionId}&x-vercel-set-bypass-cookie=true&x-vercel-protection-bypass=12345678901234567890123456789009`)
+    fetch(`https://myinnertune-qzpmn918s-tobiaswedler-2383s-projects.vercel.app/api/verify-session?session_id=${sessionId}&x-vercel-set-bypass-cookie=true&x-vercel-protection-bypass=12345678901234567890123456789009`)
       .then(res => res.json())
       .then(data => setStatus(data.message || 'Error'))
       .catch(() => setStatus('Error'));
